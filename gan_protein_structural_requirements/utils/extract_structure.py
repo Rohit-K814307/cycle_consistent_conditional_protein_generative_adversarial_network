a4c84ba2c426af9c6a174557122d51c76382051c
Rohit Kulkarni <kulkarni.rohitva@gmail.com>
2023-12-08 01:44:19 -0500

    data download and load fixed

--- gan_protein_structural_requirements/utils/extract_structure.py
+++ gan_protein_structural_requirements/utils/extract_structure.py
@@ -15,58 +15,59 @@ def extract_secondary_structure(fnames, fpaths):
     """
 
     structures = {}
-
+    bad_ids = []
     #secondary structure (c8 OR c3 dssp) - we can use either doesnt really matter just collect both for now
     for idx in range(len(fnames)):
         fname = fnames[idx]
         fpath = fpaths[idx]
+        try:
+            p = PDBParser(QUIET=True)
+            structure = p.get_structure(fname, fpath)
+            model = structure[0]
+            dssp = DSSP(model, fpath)
 
-        p = PDBParser(QUIET=True)
-        structure = p.get_structure(fname, fpath)
-        model = structure[0]
-        dssp = DSSP(model, fpath)
+            # extract sequence and secondary structure from the DSSP tuple
+            sequence = ''
+            sec_structure = ''
+            for z in range(len(dssp)):
+                a_key = list(dssp.keys())[z]
+                sequence += dssp[a_key][1]
+                sec_structure += dssp[a_key][2]
 
-        # extract sequence and secondary structure from the DSSP tuple
-        sequence = ''
-        sec_structure = ''
-        for z in range(len(dssp)):
-            a_key = list(dssp.keys())[z]
-            sequence += dssp[a_key][1]
-            sec_structure += dssp[a_key][2]
+            #
+            # The DSSP codes for secondary structure used here are:
+            # =====     ====
+            # Code      Structure
+            # =====     ====
+            # H         Alpha helix (4-12)
+            # B         Isolated beta-bridge residue
+            # E         Strand
+            # G         3-10 helix
+            # I         Pi helix
+            # T         Turn
+            # S         Bend
+            # ~         None
+            # =====     ====
+            #
+            
+            sec_structure = sec_structure.replace('-', '~')
+            sec_structure_3state=sec_structure
 
-        #
-        # The DSSP codes for secondary structure used here are:
-        # =====     ====
-        # Code      Structure
-        # =====     ====
-        # H         Alpha helix (4-12)
-        # B         Isolated beta-bridge residue
-        # E         Strand
-        # G         3-10 helix
-        # I         Pi helix
-        # T         Turn
-        # S         Bend
-        # ~         None
-        # =====     ====
-        #
-        
-        sec_structure = sec_structure.replace('-', '~')
-        sec_structure_3state=sec_structure
-
-        # if desired, convert DSSP's 8-state assignments into 3-state [C - coil, E - extended (beta-strand), H - helix]
-        sec_structure_3state = sec_structure_3state.replace('H', 'H') #0
-        sec_structure_3state = sec_structure_3state.replace('E', 'E')
-        sec_structure_3state = sec_structure_3state.replace('T', '~')
-        sec_structure_3state = sec_structure_3state.replace('~', '~')
-        sec_structure_3state = sec_structure_3state.replace('B', 'E')
-        sec_structure_3state = sec_structure_3state.replace('G', 'H') #5
-        sec_structure_3state = sec_structure_3state.replace('I', 'H') #6
-        sec_structure_3state = sec_structure_3state.replace('S', '~')
+            # if desired, convert DSSP's 8-state assignments into 3-state [C - coil, E - extended (beta-strand), H - helix]
+            sec_structure_3state = sec_structure_3state.replace('H', 'H') #0
+            sec_structure_3state = sec_structure_3state.replace('E', 'E')
+            sec_structure_3state = sec_structure_3state.replace('T', '~')
+            sec_structure_3state = sec_structure_3state.replace('~', '~')
+            sec_structure_3state = sec_structure_3state.replace('B', 'E')
+            sec_structure_3state = sec_structure_3state.replace('G', 'H') #5
+            sec_structure_3state = sec_structure_3state.replace('I', 'H') #6
+            sec_structure_3state = sec_structure_3state.replace('S', '~')
 
 
-        structures[fname] = {"sequence":sequence, "c8":sec_structure, "c3":sec_structure_3state}
-
-    return structures
+            structures[fname] = {"sequence":sequence, "c8":sec_structure, "c3":sec_structure_3state}
+        except:
+            bad_ids.append(fname)
+    return structures, bad_ids
 
 
 def find_seq_polarity(sequence):
@@ -91,28 +92,31 @@ def extract_primary_polarity(fnames, fpaths):
     """
 
     polarities = {}
+    bad_ids = []
 
     for idx in range(len(fnames)):
         fname = fnames[idx]
         fpath = fpaths[idx]
 
-        p = PDBParser(QUIET=True)
-        structure = p.get_structure(fname, fpath)
-        model = structure[0]
-        dssp = DSSP(model, fpath)
+        try:
+            p = PDBParser(QUIET=True)
+            structure = p.get_structure(fname, fpath)
+            model = structure[0]
+            dssp = DSSP(model, fpath)
 
-        # extract sequence from DSSP tuple
-        sequence = ''
-        for z in range(len(dssp)):
-            a_key = list(dssp.keys())[z]
-            sequence += dssp[a_key][1]
-        
-        #find polarity seq from AA seq
-        polarity_conv = find_seq_polarity(sequence)
+            # extract sequence from DSSP tuple
+            sequence = ''
+            for z in range(len(dssp)):
+                a_key = list(dssp.keys())[z]
+                sequence += dssp[a_key][1]
+            
+            #find polarity seq from AA seq
+            polarity_conv = find_seq_polarity(sequence)
 
-        polarities[fname] = {"sequence":sequence, "polarity_conv":polarity_conv}
-
-    return polarities
+            polarities[fname] = {"sequence":sequence, "polarity_conv":polarity_conv}
+        except:
+            bad_ids.append(fname)
+    return polarities, bad_ids
 
 def dssp_pdb(pdb_batch):
     """
@@ -217,12 +221,11 @@ def extract_structures(dir_path):
             fnames.append(file.split(".")[0])
             fpaths.append(os.path.join(dir_path, file))
 
-    secondary_structure = extract_secondary_structure(fnames, fpaths)
+    secondary_structure, _ = extract_secondary_structure(fnames, fpaths)
     structures["secondary"] = secondary_structure
     
-    polarity = extract_primary_polarity(fnames, fpaths)
+    polarity, _ = extract_primary_polarity(fnames, fpaths)
     structures["primary_pol"] = polarity
-
 
     return structures
     
