d5cd39e3fa7661aa9b4a058a1ec4bc3a859451c6
Rohit Kulkarni <kulkarni.rohitva@gmail.com>
2024-01-04 10:14:14 -0500

    update seq to vec model

--- gan_protein_structural_requirements/models/seqtovec_model.py
+++ gan_protein_structural_requirements/models/seqtovec_model.py
@@ -48,9 +48,9 @@ class SeqToVecModel(nn.Module):
 
         """
 
-        self.X = input["Y"]
+        self.X = input["Y"].float().permute(0,2,1)
 
-        self.y = input["X"]
+        self.y = input["X"][:,0,:].float()
 
         self.ids = input["IDS"]
 
@@ -68,29 +68,29 @@ class SeqToVecModel(nn.Module):
         self.y_hat = self.net(self.X,temperature)
 
         #compute scores for later visualization
-        self.mse_ahelix = self.score_metric(self.y_hat.detach()[:,0],self.X.detach()[:,0])
-        self.mse_betabridge = self.score_metric(self.y_hat.detach()[:,1],self.X.detach()[:,1])
-        self.mse_strand = self.score_metric(self.y_hat.detach()[:,2],self.X.detach()[:,2])
-        self.mse_310helix = self.score_metric(self.y_hat.detach()[:,3],self.X.detach()[:,3])
-        self.mse_pi = self.score_metric(self.y_hat.detach()[:,4],self.X.detach()[:,4])
-        self.mse_turn = self.score_metric(self.y_hat.detach()[:,5],self.X.detach()[:,5])
-        self.mse_bend = self.score_metric(self.y_hat.detach()[:,6],self.X.detach()[:,6])
-        self.mse_none = self.score_metric(self.y_hat.detach()[:,7],self.X.detach()[:,7])
-        self.mse_pol = self.score_metric(self.y_hat.detach()[:,8],self.X.detach()[:,8])
+        self.mse_ahelix = self.score_metric(self.y_hat.detach()[:,0],self.y.detach()[:,0])
+        self.mse_betabridge = self.score_metric(self.y_hat.detach()[:,1],self.y.detach()[:,1])
+        self.mse_strand = self.score_metric(self.y_hat.detach()[:,2],self.y.detach()[:,2])
+        self.mse_310helix = self.score_metric(self.y_hat.detach()[:,3],self.y.detach()[:,3])
+        self.mse_pi = self.score_metric(self.y_hat.detach()[:,4],self.y.detach()[:,4])
+        self.mse_turn = self.score_metric(self.y_hat.detach()[:,5],self.y.detach()[:,5])
+        self.mse_bend = self.score_metric(self.y_hat.detach()[:,6],self.y.detach()[:,6])
+        self.mse_none = self.score_metric(self.y_hat.detach()[:,7],self.y.detach()[:,7])
+        self.mse_pol = self.score_metric(self.y_hat.detach()[:,8],self.y.detach()[:,8])
 
 
     def backward(self):
 
         #find loss of the network
-        self.loss_net = self.net_loss(self.y_hat, self.X)
+        self.loss_net = self.net_loss(self.y_hat, self.y)
 
         #send loss backward to compute gradients
         self.loss_net.backward()
 
 
-    def optimize_parameters(self):
+    def optimize_parameters(self, temperature):
 
-        self.forward()
+        self.forward(temperature=temperature)
 
         self.optim.zero_grad()
 
